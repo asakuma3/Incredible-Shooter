87ae8ee23143b1aea80d888c93913b20ede5ec7e
asakuma3 <qjbtq324@yahoo.co.jp>
2021-10-19 21:34:32 +0900

    enemyを追加

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum RESPAWN_TYPE
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT,
+        SIZEOF,
+    }
+}
